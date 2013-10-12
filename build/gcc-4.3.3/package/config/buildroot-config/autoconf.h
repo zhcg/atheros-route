af696438ecdc64c492934f3c0b0db12fbeb8dbac
luo.dongping <luo.dongping@handaer.com>
2013-10-12 01:22:56 -0400

    initial version 2 for add git miss file in build and linux Directory

---
+++ build/gcc-4.3.3/package/config/buildroot-config/autoconf.h
@@ -0,0 +1,95 @@
+/*
+ * Automatically generated C config: don't edit
+ * Wed Oct 14 03:50:45 2009
+ */
+#define BR2_DEBIAN_MIRROR "http://ftp.debian.org"
+#define BR2_COPYTO ""
+#define BR2_GIT "git clone"
+#define BR2_GCC_SHARED_LIBGCC 1
+#define BR2_SVN_CO "svn co"
+#define BR2_EXTRA_BINUTILS_CONFIG_OPTIONS ""
+#define BR2_GNU_BUILD_SUFFIX "pc-linux-gnu"
+#define BR2_BINUTILS_VERSION "2.19.1"
+#define BR2_HOST_FAKEROOT 1
+#define BR2_TARGET_ROOTFS_EXT2_RESBLKS 0
+#define BR2_KERNEL_MIRROR "http://www.kernel.org/pub/"
+#define BR2_SVN_UP "svn up"
+#define BR2_TOPDIR_PREFIX ""
+#define BR2_EXT_UCLIBC_VERSION_0_9_30_1 1
+#define BR2_TOPDIR_SUFFIX ""
+#define BR2_AT91_PATCH_MIRROR "http://maxim.org.za/AT91RM9200/2.6/"
+#define BR2_GCC_VERSION_4_3_3 1
+#define BR2_EXT_UCLIBC_VERSION_0_9_28_3 1
+#define BR2_ROOTFS_PREFIX "rootfs"
+#define BR2_KERNEL_HEADERS_2_6_29 1
+#define BR2_ROOTFS_SUFFIX ""
+#define BR2_SOFT_FLOAT 1
+#define BR2_TARGET_ROOTFS_EXT2_NONE 1
+#define BR2_PROJECT "uclibc"
+#define BR2_MIPS_OABI32 1
+#define BR2_GCC_TARGET_ABI "32"
+#define BR2_ARCH "mips"
+#define BR2_UCLIBC_VERSION_0_9_30 1
+#define BR2_mips_32r2 1
+#define BR2_OPTIMIZE_S 1
+#define BR2_STRIP_strip 1
+#define BR2_GNU_MIRROR "http://ftp.gnu.org/pub/gnu"
+#define BR2_VERSION "2009.08"
+#define BR2_ZCAT "gzip -d -c"
+#define BR2_WGET "wget --passive-ftp -nd"
+#define BR2_TOOLCHAIN_SYSROOT 1
+#define BR2_GNU_TARGET_SUFFIX "linux-uclibc"
+#define BR2_BACKUP_SITE "http://buildroot.net/downloads/sources/"
+#define BR2_STAGING_DIR "$(BUILD_DIR)/staging_dir"
+#define BR2_EXTRA_GCC_CONFIG_OPTIONS ""
+#define BR2_GDB_VERSION "6.8"
+#define BR2_GCC_VERSION "4.3.3"
+#define BR2_EXT_GCC_VERSION_4_2_1 1
+#define BR2_EXT_GCC_VERSION_4_1_2 1
+#define BR2_EXT_GCC_VERSION_4_2_2 1
+#define BR2_EXT_GCC_VERSION_4_2_3 1
+#define BR2_GCC_SUPPORTS_SYSROOT 1
+#define BR2_HOSTNAME "uclibc"
+#define BR2_UCLIBC_VERSION_STRING "0.9.30"
+#define BR2_EXT_UCLIBC_VERSION_0_9_30 1
+#define BR2_TAR_OPTIONS ""
+#define BR2_INET_RPC 1
+#define BR2_BZCAT "bzcat"
+#define BR2_EXT_UCLIBC_VERSION_0_9_29 1
+#define BR2_GDB_VERSION_6_8 1
+#define BR2_SOURCEFORGE_MIRROR "easynews"
+#define BR2_HAVE_DOT_CONFIG 1
+#define BR2_DEFAULT_KERNEL_HEADERS "2.6.29.6"
+#define BR2_TARGET_ROOTFS_EXT2 1
+#define BR2_ROOTFS_POST_BUILD_SCRIPT ""
+#define BR2_LARGEFILE 1
+#define BR2_GCC_TARGET_TUNE "mips32r2"
+#define BR2_EXT_BINUTILS_VERSION_2_17 1
+#define BR2_CCACHE 1
+#define BR2_EXT_BINUTILS_VERSION_2_18 1
+#define BR2_PTHREADS_OLD 1
+#define BR2_CROSS_TOOLCHAIN_TARGET_UTILS 1
+#define BR2_TARGET_ROOTFS_EXT2_BLOCKS 0
+#define BR2_TARGET_ROOTFS_EXT2_INODES 0
+#define BR2_ATMEL_MIRROR "ftp://www.at91.com/pub/buildroot/"
+#define BR2_ENDIAN "BIG"
+#define BR2_TOOLCHAIN_BUILDROOT 1
+#define BR2_PACKAGE_GDB_HOST 1
+#define BR2_BANNER "Welcome to Buildroot"
+#define BR2_PRIMARY_SITE ""
+#define BR2_TARGET_ROOTFS_EXT2_SQUASH 1
+#define BR2_mips 1
+#define BR2_GCC_SUPPORTS_FINEGRAINEDMTUNE 1
+#define BR2_RECENT 1
+#define BR2_TARGET_ROOTFS_EXT2_COPYTO ""
+#define BR2_JLEVEL 1
+#define BR2_UCLIBC_CONFIG "toolchain/uClibc/uClibc-0.9.30.config"
+#define BR2_TARGET_OPTIMIZATION "-Os -pipe -funit-at-a-time -mips32r2 -mtune=mips23r2"
+#define BR2_PACKAGE_SSTRIP_HOST 1
+#define BR2_DL_DIR "$(BASE_DIR)/dl"
+#define BR2_KERNEL_none 1
+#define BR2_TOOLCHAIN_SOURCE 1
+#define BR2_TARGET_ROOTFS_EXT2_OUTPUT "$(IMAGE).ext2"
+#define BR2_CCACHE_DIR "$(TOOL_BUILD_DIR)/ccache-$(CCACHE_VER)/cache"
+#define BR2_UPDATE_CONFIG 1
+#define BR2_BINUTILS_VERSION_2_19_1 1
---
+++ linux/kernels/mips-linux-2.6.31/patches/readme.txt
@@ -0,0 +1,4 @@
+Refer to https://dev.openwrt.org/browser/trunk/target/linux/generic-2.6/patches-2.6.31?rev=17686
+051-squashfs_pcomp.patch - https://dev.openwrt.org/export/17686/trunk/target/linux/generic-2.6/patches-2.6.31/051-squashfs_pcomp.patch
+052-pcomp_lzma_support.patch - https://dev.openwrt.org/export/17686/trunk/target/linux/generic-2.6/patches-2.6.31/052-pcomp_lzma_support.patch
+053-squashfs_lzma.patch - https://dev.openwrt.org/export/17686/trunk/target/linux/generic-2.6/patches-2.6.31/053-squashfs_lzma.patch
