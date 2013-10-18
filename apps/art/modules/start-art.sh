36bb780e32f05b01e77fd233cf53aa7cc3050f04
luodongping <luo.dongping@handaer.com>
2013-10-18 20:51:21 +0800

    modify atheros radio test script

--- apps/art/modules/start-art.sh
+++ apps/art/modules/start-art.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 /etc/ath/apdown
-insmod /lib/modules/2.6.31/net/art-wasp.ko
+insmod /lib/modules/2.6.31/net/art-wasp-osprey.ko
 mknod /dev/dk0 c 63 0
-/bin/nart.out -console &
+mknod /dev/dk1 c 63 1
+/bin/nart.out -instance 0 -port 2390 -console &
+/bin/nart.out -instance 1 -port 2391 -console &
