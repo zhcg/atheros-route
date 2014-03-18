dbe69ace0a2c073d5cbdb9a146078f1f7d505650
chang.yongjin <chang.yongjin@handaer.com>
2014-03-18 14:41:04 +0800

    OTA

---
+++ apps/OTA_Base/our_md5.h
@@ -0,0 +1,31 @@
+/* Because MD5 may not be implemented (at least, with the same
+ * interface) on all systems, we have our own copy here.
+ */
+
+/* Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
+rights reserved.
+
+License to copy and use this software is granted provided that it
+is identified as the "RSA Data Security, Inc. MD5 Message-Digest
+Algorithm" in all material mentioning or referencing this software
+or this function.
+
+License is also granted to make and use derivative works provided
+that such works are identified as "derived from the RSA Data
+Security, Inc. MD5 Message-Digest Algorithm" in all material
+mentioning or referencing the derived work.
+
+RSA Data Security, Inc. makes no representations concerning either
+the merchantability of this software or the suitability of this
+software for any particular purpose. It is provided "as is"
+without express or implied warranty of any kind.
+
+These notices must be retained in any copies of any part of this
+documentation and/or software.
+ */
+
+#ifndef _SYS_MD5_H_
+#define _SYS_MD5_H_
+
+extern int CalcFileMD5(char *filename, char *md5_sum);
+#endif /* _SYS_MD5_H_ */
