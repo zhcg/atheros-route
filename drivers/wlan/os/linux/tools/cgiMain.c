480ab03c2a7ca192ed27058bc70955433e603bd7
wangyu <wang.yu0@handaer.com>
2014-04-11 17:51:08 +0800

    wangyu modify cgiMain.c for dhcp server

--- drivers/wlan/os/linux/tools/cgiMain.c
+++ drivers/wlan/os/linux/tools/cgiMain.c
@@ -2999,7 +2999,7 @@ int set_dhcp(void)
 			//Execute_cmd("/etc/ath/apcfg",rspBuff);
 			//system("/etc/rc.d/rc.udhcpd");
 			Execute_cmd("/etc/rc.d/rc.udhcpd",rspBuff);
-			Execute_cmd("/usr/sbin/set_addr > /dev/null 2>&1",rspBuff);
+			Execute_cmd("touch /etc/ip_mac.conf && cat /etc/ip_mac.conf | grep "enable" | awk 'BEGIN{OFS=FS=" "}{print $1,$2,$3}' >> /etc/udhcpd.conf",rspBuff);
 			Execute_cmd("/usr/sbin/udhcpd /etc/udhcpd.conf",rspBuff);
 		}
 		return 0;
@@ -3068,9 +3068,6 @@ int set_addr_bind(void)
 			}
 			fclose(fileBuf2);
 		}
-				
-		CFG_remove_by_name("ADD_MAC");
-		writeParameters("/tmp/.apcfg","w+",0);
 		
 		Execute_cmd("/usr/sbin/udhcpd /etc/udhcpd.conf",rspBuff);
 		return result;
@@ -6951,7 +6948,7 @@ int main(int argc,char **argv)
 				{
 					system("killall udhcpd > /dev/null 2>&1");			 
 					system("/etc/rc.d/rc.udhcpd > /dev/null 2>&1");
-					system("/usr/sbin/set_addr > /dev/null 2>&1");		 
+					system("touch /etc/ip_mac.conf && cat /etc/ip_mac.conf | grep "enable" | awk 'BEGIN{OFS=FS=" "}{print $1,$2,$3}' >> /etc/udhcpd.conf");					
 					system("/usr/sbin/udhcpd /etc/udhcpd.conf > /dev/null 2>&1");
 				}
 
