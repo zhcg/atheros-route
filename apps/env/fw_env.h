b306cfd1edc5718d7496f7955a8add50df36924f
wangyu <wang.yu0@handaer.com>
2014-04-12 14:50:56 +0800

    env problem

--- apps/env/fw_env.h
+++ apps/env/fw_env.h
@@ -42,13 +42,11 @@
 #define CONFIG_BAUDRATE		115200
 #define CONFIG_BOOTDELAY	5	/* autoboot after 5 seconds	*/
 /*wangyu modify for bootargs*/
-#define CONFIG_IPADDR	\
-		"192.168.1.1"
-#define CONFIG_SERVERIP	\
-		"192.168.1.2"
-#define CONFIG_ETHADDR   "0x00:0xaa:0xbb:0xcc:0xdd:0xee"
+#define CONFIG_IPADDR	192.168.1.1
+#define CONFIG_SERVERIP	192.168.1.2
+#define CONFIG_ETHADDR   0x00:0xaa:0xbb:0xcc:0xdd:0xee
 #define CONFIG_BOOTCOMMAND_LU	\
-	"tftp 0x80060000 ${dir}u-boot.bin&&erase 0x9f000000 +$filesize&&cp.b $fileaddr 0x9f000000 $filesize"
+		"tftp 0x80060000 ${dir}u-boot.bin&&erase 0x9f000000 +$filesize&&cp.b $fileaddr 0x9f000000 $filesize"
 #define CONFIG_BOOTCOMMAND_LK	\
 		"tftp 0x80060000 ${dir}vmlinux${bc}.lzma.uImage&&erase 0x9fe80000 +$filesize&&cp.b $fileaddr 0x9fe80000 $filesize"
 #define CONFIG_BOOTCOMMAND_LF	\
