5cc7b3fe43feba0bba1854f758aba684164112be
wangyu <wang.yu0@handaer.com>
2014-04-17 18:12:25 +0800

    cgimain.c for factory reset

--- drivers/wlan/os/linux/tools/cgiMain.c
+++ drivers/wlan/os/linux/tools/cgiMain.c
@@ -5214,7 +5214,9 @@ int main(int argc,char **argv)
 			//MAC and BACKUP
 			system("/usr/sbin/var_backup > /dev/null 2>&1");
 			system("cat /dev/null > /etc/ath/iptables/parc");
-			system("rm -f /etc/ip_mac.conf");//wangyu add for ip and mac address bond operation
+//			system("rm -f /etc/ip_mac.conf");//wangyu add for ip and mac address bond operation
+			system("echo `grep 10.10.10.100 /etc/ip_mac.conf` > /etc/ip_mac.conf");//wangyu add for ip and mac address bond operation
+
 			system("rm -f /etc/.staAcl /etc/.staMac");//wangyu add for wireless client manage
 			system("rm -f  /etc/arp_ip_mac_on.conf /etc/arp_ip_mac_off.conf /etc/arp_ip_mac.conf");//wangyu add for arp  ip and mac address bond operation
 			system("rm -f /etc/route.conf");//wangyu add for static route list
