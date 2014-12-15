d5262486b0515f8210f3d80e9cc6a47e5b5f1c36
wangyu <wang.yu0@handaer.com>
2014-12-15 17:12:59 +0800

    modify these files for arp and pppoe problem

--- apps/webui/set_sn.sh
+++ apps/webui/set_sn.sh
@@ -4,7 +4,7 @@ SN=`fw_printenv | grep SN | awk -F'=' '{print $2}'`
 sn_len=`expr length "$SN"`
 #echo $sn_len
 if [ "$sn_len" -eq  18 -a ! -e /configure_backup/.sn_backup ];then
-	echo "WRITE IN"
+	#echo "WRITE IN"
 	echo $SN > /configure_backup/.sn_backup
 fi
 
@@ -14,6 +14,6 @@ if [ -e /configure_backup/.sn_backup ];then
 	sn_backup_len=`expr length "$SN_backup"`
 	if [ "$sn_len" -ne  18 -a  "$sn_backup_len" -eq  18  ];then
 		fw_setenv SN $SN_backup
-		echo "WRITE OUT"
+		#echo "WRITE OUT"
 	fi
 fi
--- drivers/wlan/os/linux/tools/filelist.h
+++ drivers/wlan/os/linux/tools/filelist.h
@@ -8,6 +8,7 @@ char backup_file_list[10][255] =
     "/configure_backup/ip_mac.conf",
     "/configure_backup/route.conf",
     "/configure_backup/arp_ip_mac.conf",
+    "/configure_backup/arp_ip_mac_on.conf",
     "/configure_backup/ath/iptables/portmap",
     "/configure_backup/ath/iptables/parc",
     "/configure_backup/pwd.xml"
