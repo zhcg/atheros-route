a8f96207b4018a692f218ab0753c2a9385cb8817
yao.mingyue <yao.mingyue@handaer.com>
2015-03-25 10:53:09 +0800

    modify ota

--- rootfs/cgiCommon/usr/www/style/getwk.js
+++ rootfs/cgiCommon/usr/www/style/getwk.js
@@ -32,3 +32,29 @@ function ajaxget()
 	};
 	
 }
+function ajaxget_2() 
+{
+	this.seturl = function(url) 
+	{ 
+		this.po=window.location.protocol+"//"+window.location.host+"/"+url; 
+		this.init(); 
+	}
+	this.init = function(xcb) 
+	{
+		try { this.request = new XMLHttpRequest(); } 
+		catch(e1) 
+		{
+			try { this.request = new ActiveXObject("Msxml2.XMLHTTP"); } 
+			catch(e2) 
+			{
+				try { this.request = new ActiveXObject("Microsoft.XMLHTTP"); } 
+				catch(e3) { return; }
+			}
+		};
+		this.request.onreadystatechange=xcb;
+		this.request.open("GET",this.po,true); 
+		this.request.send(null);
+		
+	};
+	
+}
